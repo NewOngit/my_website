07adba038dca1d0610c9098bba70a01a66c8e566
new <uday45855@gmail.com>
2024-12-01 14:52:47 +0530

    modifying

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer =()=>{
 let date=new Date();
 return(
     <div style={{textAlign:'center',backgroundColor:'#4284f5'}}>
-        <h1>copyright &copy; {date.getFullYear()}</h1>
+        
         </div>
 )
 }
